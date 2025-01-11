a0852f834c56642fb8b946a795ab67d47f7495f6
Andrey Oleynik <andrei.oleynik@gmail.com>
2025-01-11 20:12:31 +0100

    feat: Add new component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+function ProductCard() {
+  return (
+    <article>
+      <h2>iPhone 15 Pro</h2>
+      <img
+        src="images/iphone.png"
+        alt="iPhone 15 Pro"
+        width="128px"
+        height="128px"
+      />
+      <p>Specification:</p>
+      <ul>
+        <li>A17 Pro chip with 6-core GPU</li>
+        <li>3x or 5x Telephoto camera</li>
+        <li>Up to 29 hours video playback</li>
+      </ul>
+      <button>Buy (From $999)</button>
+    </article>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductCard/>
     </div>
   );
 }
