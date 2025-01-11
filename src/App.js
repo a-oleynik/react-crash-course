6d36bf928c242d414e359a1306d52c66ac8e4f52
Andrey Oleynik <andrei.oleynik@gmail.com>
2025-01-11 21:18:31 +0100

    feat: Passing props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 import {ProductCard} from "./components/ProductCard";
 
 function App() {
+  const product = {
+    title: "iPhone 15 Pro",
+    imageSrc: "images/iphone.png",
+    specifications: [
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback",
+    ],
+    price: 999,
+  };
+
   return (
     <div className="App">
-      <ProductCard/>
+      <ProductCard product = {product}/>
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,4 @@
-export function ProductCard() {
-  const product = {
-    title: "iPhone 15 Pro",
-    imageSrc: "images/iphone.png",
-    specifications: [
-      "A17 Pro chip with 6-core GPU",
-      "3x or 5x Telephoto camera",
-      "Up to 29 hours video playback",
-    ],
-    price: 999,
-  };
-
+export function ProductCard(props) {
   return (
     <article style={{
       border: "1px solid white",
@@ -17,20 +6,20 @@ export function ProductCard() {
       padding: "16px",
       textAlign: "center"
     }}>
-      <h2> {product.title}</h2>
+      <h2> {props.product.title}</h2>
       <img
-        src={product.imageSrc}
+        src={props.product.imageSrc}
         alt="iPhone 15 Pro"
         width="128px"
         height="128px"
       />
       <p>Specification:</p>
       <ul style={{listStyle: "none", padding: 0}}>
-        <li>{product.specifications[0]}</li>
-        <li>{product.specifications[1]}</li>
-        <li>{product.specifications[2]}</li>
+        <li>{props.product.specifications[0]}</li>
+        <li>{props.product.specifications[1]}</li>
+        <li>{props.product.specifications[2]}</li>
       </ul>
-      <button>Buy (From ${product.price})</button>
+      <button>Buy (From ${props.product.price})</button>
     </article>
   )
     ;
