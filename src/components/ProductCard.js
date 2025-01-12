48ed7b96dc7aba278ca46f5c62105c3eaf6e58ce
Andrey Oleynik <andrei.oleynik@gmail.com>
2025-01-13 00:40:58 +0100

    feat: Props destruction

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,26 +1,27 @@
-export function ProductCard(props) {
+export function ProductCard({product, background = "slategrey"}) {
   return (
     <article style={{
+      background,
       width: "100%",
       border: "1px solid white",
       borderRadius: "8px",
       padding: "16px",
       textAlign: "center"
     }}>
-      <h2> {props.product.title}</h2>
+      <h2> {product.title}</h2>
       <img
-        src={props.product.imageSrc}
+        src={product.imageSrc}
         alt="iPhone 15 Pro"
         width="128px"
         height="128px"
       />
       <p>Specification:</p>
       <ul style={{listStyle: "none", padding: 0}}>
-        <li>{props.product.specifications[0]}</li>
-        <li>{props.product.specifications[1]}</li>
-        <li>{props.product.specifications[2]}</li>
+        <li>{product.specifications[0]}</li>
+        <li>{product.specifications[1]}</li>
+        <li>{product.specifications[2]}</li>
       </ul>
-      <button>Buy (From ${props.product.price})</button>
+      <button>Buy (From ${product.price})</button>
     </article>
   )
     ;
